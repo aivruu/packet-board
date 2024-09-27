7e33c4ca9ab68bf4e06644c42f86562312e2dc5d
aivruu <benjaalejandro206@gmail.com>
2024-09-26 23:27:08 -0300

    feat(PlayerRegistryListener): Include listener for player-events to handle scoreboards-registry

---
+++ plugin/src/main/java/io/github/aivruu/packetboard/listener/PlayerRegistryListener.java
@@ -0,0 +1,54 @@
+// This file is part of packet-board, licensed under the GNU License.
+//
+// Copyright (c) 2024 aivruu
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+package io.github.aivruu.packetboard.listener;
+
+import io.github.aivruu.packetboard.config.ConfigurationProvider;
+import io.github.aivruu.packetboard.config.object.SettingsConfigModel;
+import io.github.aivruu.packetboard.factory.ScoreboardFactory;
+import io.github.aivruu.packetboard.manager.BoardManager;
+import net.kyori.adventure.text.logger.slf4j.ComponentLogger;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class PlayerRegistryListener implements Listener {
+  private final ComponentLogger logger;
+  private final BoardManager boardManager;
+  private final ScoreboardFactory scoreboardFactory;
+  private final ConfigurationProvider<SettingsConfigModel> settingsConfigProvider;
+
+  public PlayerRegistryListener(final ComponentLogger logger, final BoardManager boardManager,
+                                final ScoreboardFactory scoreboardFactory, final ConfigurationProvider<SettingsConfigModel> settingsConfigProvider) {
+    this.logger = logger;
+    this.boardManager = boardManager;
+    this.scoreboardFactory = scoreboardFactory;
+    this.settingsConfigProvider = settingsConfigProvider;
+  }
+
+  @EventHandler
+  public void onJoin(final PlayerJoinEvent event) {
+    this.scoreboardFactory.create(event.getPlayer(), this.settingsConfigProvider.configModel().mode);
+  }
+
+  @EventHandler
+  public void onQuit(final PlayerQuitEvent event) {
+    if (!this.boardManager.delete(event.getPlayer())) {
+      this.logger.error("An error occurred while deleting the board for player");
+    }
+  }
+}
