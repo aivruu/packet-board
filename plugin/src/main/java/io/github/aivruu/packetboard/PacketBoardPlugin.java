aabc903443c231dbb2ebe372ed9887d44f443d8c
aivruu <benjaalejandro206@gmail.com>
2024-09-29 22:56:48 -0300

    chore: Define reusable static-fields, rename 'checkInvalidSettings' function 'checkModeType', simplify commands-registration structure, and implement additional logic to reload-method for threads' attributes update and more

--- plugin/src/main/java/io/github/aivruu/packetboard/PacketBoardPlugin.java
+++ plugin/src/main/java/io/github/aivruu/packetboard/PacketBoardPlugin.java
@@ -19,6 +19,7 @@ package io.github.aivruu.packetboard;
 import io.github.aivruu.packetboard.board.RuntimeScoreboardMode;
 import io.github.aivruu.packetboard.command.MainCommand;
 import io.github.aivruu.packetboard.command.RegistrableCommandModel;
+import io.github.aivruu.packetboard.command.ScoreboardControlCommand;
 import io.github.aivruu.packetboard.config.ConfigurationProvider;
 import io.github.aivruu.packetboard.config.object.MessagesConfigModel;
 import io.github.aivruu.packetboard.config.object.SettingsConfigModel;
@@ -33,16 +34,21 @@ import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.logger.slf4j.ComponentLogger;
+import net.luckperms.api.LuckPermsProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PacketBoardPlugin extends JavaPlugin implements PacketBoard {
+  public static final byte SUCCESSFUL_RELOAD_STATUS = 0;
+  public static final byte THREAD_SHUTDOWN_ANOMALY_STATUS = 1;
+  public static final byte CONFIGURATION_RELOAD_ANOMALY_STATUS = 2;
+  private static final String TITLE_ANIMATION_EXECUTOR_NAME = "scoreboard-title-animation-thread";
+  private static final String LINES_REFRESHER_EXECUTOR_NAME = "scoreboard-lines-updater-thread";
   private ComponentLogger logger;
   private ConfigurationProvider<SettingsConfigModel> settingsConfigProvider;
   private ConfigurationProvider<MessagesConfigModel> messagesConfigProvider;
   private ThreadExecutorRepositoryModel threadExecutorRepository;
   private BoardRepositoryModel boardRepository;
   private BoardManager boardManager;
-  private ScoreboardFactory scoreboardFactory;
 
   @Override
   public BoardRepositoryModel boardRepository() {
@@ -69,32 +75,37 @@ public final class PacketBoardPlugin extends JavaPlugin implements PacketBoard {
       this.logger.error(Component.text("Some, or both configuration files couldn't be loaded.").color(NamedTextColor.YELLOW));
       return;
     }
-    if (this.checkInvalidSettings()) {
-      this.logger.error(Component.text("If you selected the 'GROUP' mode, you require have LuckPerms installed. Otherwise, you specified an invalid update-rate value for the title-animation.").color(NamedTextColor.YELLOW));
+    if (this.checkModeType()) {
+      this.logger.error(Component.text("You've selected the 'GROUP' mode, it requires have LuckPerms installed.").color(NamedTextColor.YELLOW));
       return;
     }
     PacketBoardProvider.set(this);
+    // Main plugin APIs and controllers initialization process.
     this.threadExecutorRepository = new ThreadExecutorRepositoryModel(this.logger);
     this.registerThreadExecutors();
     this.logger.info(Component.text("Initialized and cached required thread-executors.").color(NamedTextColor.YELLOW));
     this.boardRepository = new BoardRepositoryModel();
     this.boardManager = new BoardManager(this.boardRepository);
-    this.scoreboardFactory = new ScoreboardFactory(this.boardManager, this.settingsConfigProvider, this.messagesConfigProvider);
     this.logger.info(Component.text("Initialized API main scoreboard controllers.").color(NamedTextColor.YELLOW));
-    this.registerCommands(new MainCommand(this.boardManager));
+    // Commands registration process.
+    this.registerCommands(
+      new MainCommand(this, this.messagesConfigProvider),
+      new ScoreboardControlCommand(this.boardManager, this.messagesConfigProvider, this.settingsConfigProvider));
     this.logger.info(Component.text("Registered commands.").color(NamedTextColor.YELLOW));
+    // Listeners registration process.
+    final var scoreboardFactory = new ScoreboardFactory(this.boardManager, this.settingsConfigProvider,
+      LuckPermsProvider.get().getUserManager());
     super.getServer()
       .getPluginManager()
-      .registerEvents(new PlayerRegistryListener(this.logger, this.boardManager, this.scoreboardFactory, this.settingsConfigProvider), this);
+      .registerEvents(new PlayerRegistryListener(this.logger, this.boardManager, scoreboardFactory, this.settingsConfigProvider), this);
     this.logger.info(Component.text("Registered event listener.").color(NamedTextColor.AQUA));
     this.logger.info(Component.text("Plugin enabled!").color(NamedTextColor.GREEN));
   }
 
-  private boolean checkInvalidSettings() {
+  private boolean checkModeType() {
     final var config = this.settingsConfigProvider.configModel();
     final var luckPermsNotInstalled = super.getServer().getPluginManager().getPlugin("LuckPerms") == null;
-    return ((config.mode == RuntimeScoreboardMode.GROUP) && !luckPermsNotInstalled)
-      || (config.enableAnimatedTitleFeature && config.animatedTitleUpdateRate > config.animatedTitleContent.length);
+    return (config.mode == RuntimeScoreboardMode.GROUP) && !luckPermsNotInstalled;
   }
 
   public void registerThreadExecutors() {
@@ -102,28 +113,55 @@ public final class PacketBoardPlugin extends JavaPlugin implements PacketBoard {
     // Only register, and start thread-executors if features are enabled from configuration.
     if (config.enableLinesRefreshing) {
       final var scoreboardLinesUpdaterThread = new AsyncLinesUpdateThread(this.boardRepository, this.settingsConfigProvider);
-      scoreboardLinesUpdaterThread.schedule(config.linesUpdateRate);
+      scoreboardLinesUpdaterThread.schedule(config.linesUpdateRateInTicks);
       this.threadExecutorRepository.saveSync(scoreboardLinesUpdaterThread);
     }
     if (config.enableAnimatedTitleFeature) {
       final var scoreboardTitleAnimationThread = new AsyncTitleAnimationThread(this.boardRepository, config.animatedTitleContent);
-      scoreboardTitleAnimationThread.schedule(config.animatedTitleUpdateRate);
+      scoreboardTitleAnimationThread.schedule(config.animatedTitleUpdateRateInTicks);
       this.threadExecutorRepository.saveSync(scoreboardTitleAnimationThread);
     }
   }
 
   @SuppressWarnings("UnstableApiUsage")
   private void registerCommands(final RegistrableCommandModel... registrableCommandModels) {
-    final var lifecycleEventManager = super.getLifecycleManager();
-    for (final var registrableCommandModel : registrableCommandModels) {
-      lifecycleEventManager.registerEventHandler(LifecycleEvents.COMMANDS, event -> {
-        final var commands = event.registrar();
-        commands.register(registrableCommandModel.register());
-      });
-    }
+    super.getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS, event -> {
+      for (final var registrableCommandModel : registrableCommandModels) {
+        // Register commands attaching its [LiteralCommandNode] and it's alias.
+        event.registrar().register(registrableCommandModel.register(), registrableCommandModel.alias());
+      }
+    });
   }
 
-  public boolean reload() {
+  public byte reload() {
+    if (this.updateConfigurations()) {
+      return CONFIGURATION_RELOAD_ANOMALY_STATUS;
+    }
+    final var config = this.settingsConfigProvider.configModel();
+    final var linesUpdateThreadExecutor = this.threadExecutorRepository.findSync(LINES_REFRESHER_EXECUTOR_NAME);
+    final var titleAnimationThreadExecutor = this.threadExecutorRepository.findSync(TITLE_ANIMATION_EXECUTOR_NAME);
+    // We check if features-that-require-threads are enable, then we update these threads' attributes.
+    // Otherwise, if these features are now disabled and threads are still active, we need shutdown them
+    // to avoid unnecessary task-processing.
+    //
+    // If the shutdown-process for the executors isn't normal, we return the status-type for these anomaly-types.
+    if (config.enableLinesRefreshing) {
+      // Cast it into it's [AsyncLinesUpdateThread] implementation for config-provider modification access.
+      // It will not be null at this point.
+      ((AsyncLinesUpdateThread) linesUpdateThreadExecutor).settingsConfigProvider(this.settingsConfigProvider);
+    } else if ((linesUpdateThreadExecutor != null) && !this.threadExecutorRepository.deleteSync(LINES_REFRESHER_EXECUTOR_NAME)) {
+      return THREAD_SHUTDOWN_ANOMALY_STATUS;
+    }
+    if (config.enableAnimatedTitleFeature) {
+      // Cast it into it's [AsyncTitleAnimationThread] implementation for content modification.
+      ((AsyncTitleAnimationThread) titleAnimationThreadExecutor).content(config.animatedTitleContent);
+    } else if ((titleAnimationThreadExecutor != null) && !this.threadExecutorRepository.deleteSync(TITLE_ANIMATION_EXECUTOR_NAME)) {
+      return THREAD_SHUTDOWN_ANOMALY_STATUS;
+    }
+    return SUCCESSFUL_RELOAD_STATUS;
+  }
+
+  public boolean updateConfigurations() {
     final var settingsModificationStatus = this.settingsConfigProvider.reload();
     final var messagesModificationStatus = this.messagesConfigProvider.reload();
     if (settingsModificationStatus.failedOnModificationStatus() || messagesModificationStatus.failedOnModificationStatus()) {
