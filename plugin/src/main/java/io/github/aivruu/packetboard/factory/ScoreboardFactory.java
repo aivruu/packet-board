243cdcf8f66931ade9e472c9df8e57a6250fe6b2
aivruu <benjaalejandro206@gmail.com>
2024-09-29 22:53:04 -0300

    chore(ScoreboardFactory): Simplify 'create' method and define logic for group-check for 'GROUP' scoreboard-mode

--- plugin/src/main/java/io/github/aivruu/packetboard/factory/ScoreboardFactory.java
+++ plugin/src/main/java/io/github/aivruu/packetboard/factory/ScoreboardFactory.java
@@ -19,71 +19,75 @@ package io.github.aivruu.packetboard.factory;
 import io.github.aivruu.packetboard.board.RuntimeScoreboardMode;
 import io.github.aivruu.packetboard.component.ComponentParserUtils;
 import io.github.aivruu.packetboard.config.ConfigurationProvider;
-import io.github.aivruu.packetboard.config.object.MessagesConfigModel;
 import io.github.aivruu.packetboard.config.object.SettingsConfigModel;
 import io.github.aivruu.packetboard.manager.BoardManager;
+import net.luckperms.api.model.user.UserManager;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 public class ScoreboardFactory {
   private final BoardManager boardManager;
   private final ConfigurationProvider<SettingsConfigModel> settingsConfigProvider;
-  private final ConfigurationProvider<MessagesConfigModel> messagesConfigProvider;
+  private final UserManager luckPermsUserManager;
 
   public ScoreboardFactory(final BoardManager boardManager, final ConfigurationProvider<SettingsConfigModel> settingsConfigProvider,
-                           final ConfigurationProvider<MessagesConfigModel> messagesConfigProvider) {
+                           final UserManager luckPermsUserManager) {
     this.boardManager = boardManager;
     this.settingsConfigProvider = settingsConfigProvider;
-    this.messagesConfigProvider = messagesConfigProvider;
+    this.luckPermsUserManager = luckPermsUserManager;
   }
 
   public void create(final Player player, final RuntimeScoreboardMode mode) {
     final var config = this.settingsConfigProvider.configModel();
-    // Variable initialization logic to determinate if, regardless of scoreboard-mode, the scoreboard was created
-    // and shown to the player.
-    var scoreboardCreated = false;
     switch (mode) {
-      case GLOBAL ->
-        // Simplified condition with ternary-operator.
-        scoreboardCreated = config.enableAnimatedTitleFeature
-          ? this.boardManager.create(player, config.animatedTitleContent[0], config.globalLines)
-          : this.boardManager.create(player, ComponentParserUtils.apply(config.globalTitle), config.globalLines);
+      case GLOBAL -> {
+        if (config.enableAnimatedTitleFeature) {
+          this.boardManager.create(player, config.animatedTitleContent[0], config.globalLines);
+        } else {
+          this.boardManager.create(player, ComponentParserUtils.apply(config.globalTitle), config.globalLines);
+        }
+      }
       // The title-animation doesn't consider specific modes. so we avoid bugs with the title.
-      case WORLD -> scoreboardCreated = !config.enableAnimatedTitleFeature && this.fromWorldSections(player);
-      case PERMISSION -> scoreboardCreated = !config.enableAnimatedTitleFeature && this.fromPermissionSections(player);
-      case GROUP -> scoreboardCreated = !config.enableAnimatedTitleFeature && this.fromGroupSections(player);
+      case WORLD -> {
+        if (!config.enableAnimatedTitleFeature) this.fromWorldSections(player);
+      }
+      case PERMISSION -> {
+        if (!config.enableAnimatedTitleFeature) this.fromPermissionSections(player);
+      }
+      case GROUP -> {
+        if (!config.enableAnimatedTitleFeature) this.fromGroupSections(player);
+      }
     };
-    // Notify errors during scoreboard's creation to the player?
-    if (config.notifyScoreboardCreationErrors && !scoreboardCreated) {
-      player.sendMessage(ComponentParserUtils.apply(this.messagesConfigProvider.configModel().scoreboardCreationFailed));
-    }
   }
 
-  private boolean fromWorldSections(final Player player) {
+  private void fromWorldSections(final Player player) {
     for (final var worldSection : this.settingsConfigProvider.configModel().scoreboardWorld) {
       if (!player.getWorld().getName().equals(worldSection.designedWorld)) continue;
-      return this.boardManager.create(player, ComponentParserUtils.apply(worldSection.title), worldSection.lines);
+      // Create scoreboard using this world-section's title and defined content.
+      this.boardManager.create(player, ComponentParserUtils.apply(worldSection.title), worldSection.lines);
     }
-    return false;
   }
 
-  private boolean fromPermissionSections(final Player player) {
+  private void fromPermissionSections(final Player player) {
     for (final var permissionSection : this.settingsConfigProvider.configModel().scoreboardPermission) {
       if (!player.hasPermission(permissionSection.node)) continue;
-      return this.boardManager.create(player, ComponentParserUtils.apply(permissionSection.title), permissionSection.lines);
+      this.boardManager.create(player, ComponentParserUtils.apply(permissionSection.title), permissionSection.lines);
     }
-    return false;
   }
 
-  private boolean fromGroupSections(final Player player) {
+  private void fromGroupSections(final Player player) {
     // This mode requires LuckPerms available for groups settings.
     if (Bukkit.getPluginManager().getPlugin("LuckPerms") == null) {
-      return false;
+      return;
     }
     for (final var groupSection : this.settingsConfigProvider.configModel().scoreboardGroup) {
+      final var luckPermsUser = this.luckPermsUserManager.getUser(player.getUniqueId());
+      // Check if player's user information is available, and its group can see this scoreboard.
+      if ((luckPermsUser == null) || !luckPermsUser.getPrimaryGroup().equals(groupSection.designedGroup)) {
+        continue;
+      }
       // Group check by LuckPerms' API.
-      return this.boardManager.create(player, ComponentParserUtils.apply(groupSection.title), groupSection.lines);
+      this.boardManager.create(player, ComponentParserUtils.apply(groupSection.title), groupSection.lines);
     }
-    return false;
   }
 }
