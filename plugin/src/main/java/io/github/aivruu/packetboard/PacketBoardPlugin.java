179ed20351495fe183718cf3208da0cf499abed5
aivruu <benjaalejandro206@gmail.com>
2024-09-30 17:10:38 -0300

    chore(plugin): Remove no-more-used code, rename thread-executors registry function to 'registerPluginTasks' and unregister it during shutdown, and simplify reload function

--- plugin/src/main/java/io/github/aivruu/packetboard/PacketBoardPlugin.java
+++ plugin/src/main/java/io/github/aivruu/packetboard/PacketBoardPlugin.java
@@ -16,6 +16,7 @@
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 package io.github.aivruu.packetboard;
 
+import io.github.aivruu.packetboard.board.CachedBoardModel;
 import io.github.aivruu.packetboard.command.MainCommand;
 import io.github.aivruu.packetboard.command.RegistrableCommandModel;
 import io.github.aivruu.packetboard.command.ScoreboardControlCommand;
@@ -26,32 +27,29 @@ import io.github.aivruu.packetboard.factory.ScoreboardFactory;
 import io.github.aivruu.packetboard.listener.PlayerRegistryListener;
 import io.github.aivruu.packetboard.manager.BoardManager;
 import io.github.aivruu.packetboard.board.BoardRepositoryModel;
-import io.github.aivruu.packetboard.thread.ThreadExecutorRepositoryModel;
-import io.github.aivruu.packetboard.thread.impl.AsyncLinesUpdateThread;
-import io.github.aivruu.packetboard.thread.impl.AsyncTitleAnimationThread;
+import io.github.aivruu.packetboard.repository.RepositoryModel;
+import io.github.aivruu.packetboard.task.LinesUpdatePluginTask;
+import io.github.aivruu.packetboard.task.TitleAnimationPluginTask;
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.logger.slf4j.ComponentLogger;
-import net.luckperms.api.LuckPermsProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.concurrent.TimeUnit;
+
 public final class PacketBoardPlugin extends JavaPlugin implements PacketBoard {
-  public static final byte SUCCESSFUL_RELOAD_STATUS = 0;
-  public static final byte THREAD_SHUTDOWN_ANOMALY_STATUS = 1;
-  public static final byte CONFIGURATION_RELOAD_ANOMALY_STATUS = 2;
-  private static final String TITLE_ANIMATION_EXECUTOR_NAME = "scoreboard-title-animation-thread";
-  private static final String LINES_REFRESHER_EXECUTOR_NAME = "scoreboard-lines-updater-thread";
   private ComponentLogger logger;
   private ConfigurationProvider<SettingsConfigModel> settingsConfigProvider;
   private ConfigurationProvider<MessagesConfigModel> messagesConfigProvider;
-  private ThreadExecutorRepositoryModel threadExecutorRepository;
-  private BoardRepositoryModel boardRepository;
+  private RepositoryModel<CachedBoardModel> boardRepository;
   private BoardManager boardManager;
   private PlayerRegistryListener scoreboardsRegistryListener;
+  private LinesUpdatePluginTask linesUpdatePluginTask;
+  private TitleAnimationPluginTask titleAnimationPluginTask;
 
   @Override
-  public BoardRepositoryModel boardRepository() {
+  public RepositoryModel<CachedBoardModel> boardRepository() {
     return this.boardRepository;
   }
 
@@ -64,7 +62,7 @@ public final class PacketBoardPlugin extends JavaPlugin implements PacketBoard {
   public void onLoad() {
     final var directory = super.getDataFolder().toPath();
     this.logger = super.getComponentLogger();
-    this.logger.info(Component.text("Loading essential plugin components...").color(NamedTextColor.AQUA));
+    this.logger.info(Component.text("Loading configurations...").color(NamedTextColor.AQUA));
     this.settingsConfigProvider = ConfigurationProvider.of(directory, "config.conf", SettingsConfigModel.class);
     this.messagesConfigProvider = ConfigurationProvider.of(directory, "messages.conf", MessagesConfigModel.class);
   }
@@ -77,12 +75,11 @@ public final class PacketBoardPlugin extends JavaPlugin implements PacketBoard {
     }
     PacketBoardProvider.set(this);
     // Main plugin APIs and controllers initialization process.
-    this.threadExecutorRepository = new ThreadExecutorRepositoryModel(this.logger);
-    this.registerThreadExecutors();
-    this.logger.info(Component.text("Initialized and cached required thread-executors.").color(NamedTextColor.YELLOW));
     this.boardRepository = new BoardRepositoryModel();
     this.boardManager = new BoardManager(this.boardRepository);
-    this.logger.info(Component.text("Initialized API main scoreboard controllers.").color(NamedTextColor.YELLOW));
+    this.logger.info(Component.text("Initialized main plugin APIs.").color(NamedTextColor.YELLOW));
+    this.registerPluginTasks();
+    this.logger.info(Component.text("Initialized necessary plugin-tasks.").color(NamedTextColor.YELLOW));
     // Commands registration process.
     this.registerCommands(
       new MainCommand(this, this.messagesConfigProvider),
@@ -95,26 +92,25 @@ public final class PacketBoardPlugin extends JavaPlugin implements PacketBoard {
 
   private void registerListener() {
     // Listeners registration process.
-    final var scoreboardFactory = new ScoreboardFactory(this.boardManager, LuckPermsProvider.get().getUserManager());
+    final var scoreboardFactory = new ScoreboardFactory(this.boardManager);
     super.getServer().getPluginManager().registerEvents(
       this.scoreboardsRegistryListener = new PlayerRegistryListener(this.logger, this.boardManager, scoreboardFactory,
         this.settingsConfigProvider.configModel()), this);
   }
 
-  private void registerThreadExecutors() {
+  private void registerPluginTasks() {
+    final var asyncScheduler = super.getServer().getAsyncScheduler();
     final var config = this.settingsConfigProvider.configModel();
     // Only register, and start thread-executors if features are enabled from configuration.
     if (config.enableLinesRefreshing) {
-      final var scoreboardLinesUpdaterThread = new AsyncLinesUpdateThread(this.boardRepository,
-        this.settingsConfigProvider.configModel());
-      // Schedule period-rate for thread and store it.
-      scoreboardLinesUpdaterThread.schedule(config.linesUpdateRateInTicks);
-      this.threadExecutorRepository.saveSync(scoreboardLinesUpdaterThread);
+      this.linesUpdatePluginTask = new LinesUpdatePluginTask(this.boardRepository, config);
+      asyncScheduler.runAtFixedRate(this, this.linesUpdatePluginTask, 0,
+        config.linesUpdateRateSeconds, TimeUnit.SECONDS);
     }
     if (config.enableAnimatedTitleFeature) {
-      final var scoreboardTitleAnimationThread = new AsyncTitleAnimationThread(this.boardRepository, config.animatedTitleContent);
-      scoreboardTitleAnimationThread.schedule(config.animatedTitleUpdateRateInTicks);
-      this.threadExecutorRepository.saveSync(scoreboardTitleAnimationThread);
+      this.titleAnimationPluginTask = new TitleAnimationPluginTask(this.boardRepository, config.animatedTitleContent);
+      asyncScheduler.runAtFixedRate(this, this.titleAnimationPluginTask, 0,
+        config.animatedTitleUpdateRateSeconds, TimeUnit.SECONDS);
     }
   }
 
@@ -128,34 +124,21 @@ public final class PacketBoardPlugin extends JavaPlugin implements PacketBoard {
     });
   }
 
-  public byte reload() {
-    if (this.updateConfigurations()) {
-      return CONFIGURATION_RELOAD_ANOMALY_STATUS;
+  public boolean reload() {
+    if (!this.updateConfigurations()) {
+      return false;
     }
     final var config = this.settingsConfigProvider.configModel();
-    final var linesUpdateThreadExecutor = this.threadExecutorRepository.findSync(LINES_REFRESHER_EXECUTOR_NAME);
-    final var titleAnimationThreadExecutor = this.threadExecutorRepository.findSync(TITLE_ANIMATION_EXECUTOR_NAME);
-    // We check if features-that-require-threads are enable, then we update these threads' attributes.
-    // Otherwise, if these features are now disabled and threads are still active, we need shutdown them
-    // to avoid unnecessary task-processing.
-    //
-    // If the shutdown-process for the executors isn't normal, we return the status-type for these anomaly-types.
+    // If features that requires periodic-tasks are enabled, we update these tasks' attributes.
     if (config.enableLinesRefreshing) {
-      // Cast it into it's [AsyncLinesUpdateThread] implementation for config-provider modification access.
-      // It will not be null at this point.
-      ((AsyncLinesUpdateThread) linesUpdateThreadExecutor).configModel(config);
-    } else if ((linesUpdateThreadExecutor != null) && !this.threadExecutorRepository.deleteSync(LINES_REFRESHER_EXECUTOR_NAME)) {
-      return THREAD_SHUTDOWN_ANOMALY_STATUS;
+      this.linesUpdatePluginTask.configModel(config);
     }
     if (config.enableAnimatedTitleFeature) {
-      // Cast it into it's [AsyncTitleAnimationThread] implementation for content modification.
-      ((AsyncTitleAnimationThread) titleAnimationThreadExecutor).content(config.animatedTitleContent);
-    } else if ((titleAnimationThreadExecutor != null) && !this.threadExecutorRepository.deleteSync(TITLE_ANIMATION_EXECUTOR_NAME)) {
-      return THREAD_SHUTDOWN_ANOMALY_STATUS;
+      this.titleAnimationPluginTask.content(config.animatedTitleContent);
     }
     // Update config-model for event-listener instance.
     this.scoreboardsRegistryListener.configModel(config);
-    return SUCCESSFUL_RELOAD_STATUS;
+    return true;
   }
 
   public boolean updateConfigurations() {
@@ -172,12 +155,10 @@ public final class PacketBoardPlugin extends JavaPlugin implements PacketBoard {
 
   @Override
   public void onDisable() {
-    if (this.threadExecutorRepository != null) {
-      this.threadExecutorRepository.clearRegistry();
-    }
     if (this.boardManager != null) {
       this.boardManager.close();
     }
+    super.getServer().getAsyncScheduler().cancelTasks(this);
     this.logger.info(Component.text("Plugin disabled!").color(NamedTextColor.RED));
   }
 }
